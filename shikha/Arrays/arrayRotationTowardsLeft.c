1d6bcebf69000b11b1ad62f006d903168ee19f4b
Shikha Suman <67411985+Shikha219@users.noreply.github.com>
2021-09-27 10:52:51 +0530

    Create arrayRotationTowardsLeft

---
+++ shikha/Arrays/arrayRotationTowardsLeft.c
@@ -0,0 +1,33 @@
+// Given an array, Mister Arr the owner of the array wants to move each element k places to its left. I.E. the array a[0],a[1],a[2],...a[n] becomes a[1],a[2]...a[n],a[0] after moving one place to the left. Note that the first element becomes the last element after each rotation. Mister Arr was up all night watching a football match and hence is sleeping now. He has given you the task to rotate the entire array K places to its left.
+//Input 5 4
+// 1 2 3 4 5
+
+//Output
+//5 1 2 3 4
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,k;
+    scanf("%d%d",&n,&k);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<k;i++){
+        int j,first = a[0];
+        for(j=0;j<n-1;j++){
+            a[j] = a[j+1];
+        }a[j] = first;
+    }
+    
+    for(int i=0;i<n;i++){
+        printf("%d",a[i]);
+    }
+    
+    return 0;
+}
