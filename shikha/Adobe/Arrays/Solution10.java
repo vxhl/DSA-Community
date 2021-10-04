78f870ed51c1f09435bf2fa4f1694f7c70dbb0d7
Shikha Suman <67411985+Shikha219@users.noreply.github.com>
2021-10-04 09:28:16 +0530

    Pythagorean Triplet

---
+++ shikha/Adobe/Arrays/Solution10.java
@@ -0,0 +1,38 @@
+/* Given an array arr of N integers, write a function that returns true if there is a triplet (a, b, c) that satisfies a2 + b2 = c2, otherwise false.
+
+Example 1:
+
+Input:
+N = 5
+Arr[] = {3, 2, 4, 6, 5}
+Output: Yes
+Explanation: a=3, b=4, and c=5 forms a
+pythagorean triplet.
+Example 2:
+
+Input:
+N = 3
+Arr[] = {3, 8, 5}
+Output: No
+Explanation: No such triplet possible. */
+
+class Solution {
+    boolean checkTriplet(int[] arr, int n) {
+        for (int i = 0; i < n; i++) arr[i] = arr[i] * arr[i];
+ 
+        Arrays.sort(arr);
+ 
+        for (int i = n - 1; i >= 2; i--) {
+            int l = 0; 
+            int r = i - 1; 
+            while (l < r) {
+                if (arr[l] + arr[r] == arr[i])
+                    return true;
+                if (arr[l] + arr[r] < arr[i])
+                    l++;
+                else
+                    r--;
+            }
+        }return false;
+    }
+}
